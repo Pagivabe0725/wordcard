dc31ed1902ac83e7a61756b6c7cc085b71a29806
Kovács <kovpatrik0725@gmail.com>
2024-08-06 17:23:04 +0200

    basic_structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'public',
+    pathMatch: 'full',
+  },
+  {
+    path: 'public',
+    component: PublicComponent,
+    title: 'public',
+  },
+  {
+    path: 'private',
+    component: PrivateComponent,
+    title: 'private',
+  },
+];
